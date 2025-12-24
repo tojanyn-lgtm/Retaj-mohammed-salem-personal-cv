a20e176dc05673470d8f49809135a5a3793ef0ee
tojanyn-lgtm <tojanyn@gmail.com>
2025-12-25 02:47:09 +0300

    Add dark mode functionality using JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+// Dark Mode Toggle
+const toggleButton = document.getElementById("toggleTheme");
+
+toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
